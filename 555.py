551e00c4f8a90a3c8a209cbdfb2bb3ee28e63d7c
MilenaDanielyan123 <m.danelyan777@gmail.com>
2026-06-24 17:57:54 +0000

    Add my file

---
+++ 555.py
@@ -0,0 +1,10 @@
+x = int(input("enter a number"))
+
+if 90 <= x <=100:
+    print("Excellent")
+elif 70 <= x <= 89:  
+    print("Good")
+elif 50 <= x <= 69:
+    print("Normal") 
+else:
+    print("Insufficent")    
