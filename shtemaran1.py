5517cfe3971e96caf193092b728e6c2075d4dcf4
MilenaDanielyan123 <m.danelyan777@gmail.com>
2026-07-04 06:22:07 +0000

    Initial commit

---
+++ shtemaran1.py
@@ -0,0 +1,20 @@
+#a = input("please enter some text") #Մուտքագրել տողը և տպել դրա երկարությունը
+#print(f"the length of the text you\'ve entered is {len(a)}")
+
+#a = input("please enter some text") #Մուտքագրել տեքստ և կրկնապատկել այն
+#print(a * 2)
+
+#n = input("please enter some text") #Մուտքագրել տեքստ և հաշվել դրա a և b տառերի քանակը։
+#n.lower()
+#a= n.count("a")
+#b = n.count("b")
+#print(f"the number of letter \"a\" encountered in the text is {a} ")
+#print(f"the number of letter \"b\" encountered in the text is {b}")
+
+#n = input("please enter some text") #Մուտքագրել տեքտ և դրա միջի բոլոր a տառերը փոխարինել #-ով։
+#a = n.replace("a",("#"))
+#print(a)
+
+n = input("please enter some text")
+a = n.replace(("a"),(" "))replace(("A"),(" "))
+print(a)
